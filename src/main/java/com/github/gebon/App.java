cfd50e6d49eae2623d975b040977ed8e6d1a0660
Gallyam Biktashev <gbiktashev@naumen.ru>
2016-09-07 11:33:04 +0500

    Build complete [2]

---
+++ src/main/java/com/github/gebon/App.java
@@ -0,0 +1,13 @@
+package com.github.gebon;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
